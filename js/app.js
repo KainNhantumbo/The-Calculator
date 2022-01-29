563a891f1a19cd374bec430b5a58ede6ff703631
KainNhantumbo <nhantumbok@gmail.com>
2022-01-29 13:27:04 +0200

    Adicionada a função de tratamento dos erros de cálculo mais comuns. Adicionada a função de cálculo de equações quadráticas.

--- js/app.js
+++ js/app.js
@@ -2,15 +2,28 @@
 import { openTabById } from './view/view.js';
 import { closeTab } from './view/view.js';
 import { calcEqLinear } from './modules/functions.js';
+import { calcEqQuadratica } from './modules/functions.js';
 
 function initializer () {
     document.getElementById('backBtn').addEventListener('click', e => closeTab());
 
+    // abre a aba de cálculo de equação linear e
+    // adiciona eventos nos inputs 
     document.getElementById('eq-linear').addEventListener('click', e => openTabById('algebra-linear'));
-    document.getElementById('algebra-linear').addEventListener('keyup', e => calcEqLinear());
-    
+    document.getElementById('algebra-linear').addEventListener('keyup', e => {
+        const event = e.target.tagName;
+        if (event === 'INPUT')
+        return calcEqLinear();
+    });
+
+    // abre a aba de cálculo de equação quadrática e
+    // adiciona eventos nos inputs 
     document.getElementById('eq-quadratica').addEventListener('click', e => openTabById('algebra-quadratica'));
-    document.getElementById('eq-quadratica').addEventListener('keyup', e => calcEqLinear());
+    document.getElementById('algebra-quadratica').addEventListener('keyup', e => {
+        const event = e.target.tagName;
+        if (event === 'INPUT')
+        return calcEqQuadratica();
+    });
 }
 
 window.addEventListener('load', initializer);
--- js/modules/functions.js
+++ js/modules/functions.js
@@ -1,35 +1,77 @@
-
+const errorHandler = (result) => {
+    switch (result) {
+        case undefined:
+            result = '';
+            break;
+        case Infinity:
+            result = '&infin;';
+            break;
+        case -Infinity:
+            result = '- &infin;';
+            break;
+        case +Infinity:
+            result = '+ &infin;';
+            break;
+        case NaN:
+            result = 'Digite números válidos';
+        default:
+            result;
+        break;
+    }
+    
+    return result;
+}
 
 export async function calcEqLinear () {
     try {
         let a = await document.getElementById('input-a').value;
         let b = await document.getElementById('input-b').value;
         let result = document.getElementById('result');
-        let x = 0;
 
-        x = -b / a;
-        return result.textContent = `x = ${x}`;
+        if (a && b !== '') {
+            const x = -b / a;
+            const resolved = errorHandler(x);
+            return result.innerHTML = `x = ${resolved}`;
+        }
     } catch (err) {
-        console.log(err);
+        return console.log(err);
     }
 }
 
-/* function calcEquQuadratic () {
-    const a = 0;
-    const b = 0;
-    const c = 0;
-    const result = 0;
-    const x1 = 0;
-    const x2 = 0;
+export async function calcEqQuadratica () {
+    try {
+        let a = await document.getElementById('input-a2').value;
+        let b = await document.getElementById('input-b2').value;
+        let c = await document.getElementById('input-c2').value;
+        let result = document.getElementById('result-2');
+        console.log('swdefsdf')
 
-    const delta = Math.pow(b, 2) - 4 * a * c;
+        if (a && b && c !== '') {
+            console.log(a, b, c)
+            const delta = Math.pow(b, 2) - 4 * a * c;
 
-    if (delta >= 0) {
-        x1 = (- b + Math.sqrt(delta)) / 2 * a;
-        x2 = (- b - Math.sqrt(delta)) / 2 * a;
-    } else if (delta === 0) {
-        result.textContent = `Delta equals to ${delta}`;
-    } else {
-        result.textContent = `Dela`
+            if (delta < 0) {
+                return result.innerHTML = `Não existem raizes reais para esta equação: ${a}x^2 + ${b}x + ${c} = 0
+                <br>&bigtriangleup; = ${delta}
+                `;
+            }
+            else if (delta == 0) {
+                return result.innerHTML = `Existem raizes iguais. <br>Solução: ${delta}`;
+            } 
+            else if (delta >= 0) {
+                const x1 = (- b + Math.sqrt(delta)) / 2 * a;
+                const x2 = (- b - Math.sqrt(delta)) / 2 * a;
+                const resolveX1 = errorHandler(x1);
+                const resolveX2 = errorHandler(x2);
+
+                return result.innerHTML = `
+                Existem duas raizes reais diferentes. <br>
+                Solução: Raíz 1: ${resolveX1}<br>Raíz 2: ${resolveX2}
+                `;
+            }
+        }
+        
+    } catch (err) {
+        return console.log(err);
     }
-} */
+}
