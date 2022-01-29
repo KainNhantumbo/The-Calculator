5e0c3393ae082b97713740dc35708669a5948bb7
KainNhantumbo <nhantumbok@gmail.com>
2022-01-29 10:16:45 +0200

    Adicionada a interface para o cálculo de equações quadráticas.

--- js/app.js
+++ js/app.js
@@ -4,10 +4,13 @@ import { closeTab } from './view/view.js';
 import { calcEqLinear } from './modules/functions.js';
 
 function initializer () {
-    document.getElementById('eq-linear').addEventListener('click', e => openTabById('algebra-linear'));
     document.getElementById('backBtn').addEventListener('click', e => closeTab());
 
-    document.getElementById('algebra-linear').addEventListener('keyup', e => calcEqLinear())
+    document.getElementById('eq-linear').addEventListener('click', e => openTabById('algebra-linear'));
+    document.getElementById('algebra-linear').addEventListener('keyup', e => calcEqLinear());
+    
+    document.getElementById('eq-quadratica').addEventListener('click', e => openTabById('algebra-quadratica'));
+    document.getElementById('eq-quadratica').addEventListener('keyup', e => calcEqLinear());
 }
 
 window.addEventListener('load', initializer);
