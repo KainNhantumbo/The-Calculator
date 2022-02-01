15d192202616adaf90fba4e118940a278943f33e
KainNhantumbo <nhantumbok@gmail.com>
2022-02-01 20:12:27 +0200

    Adicionado o operador mod.

--- js/modules/base-calculator.js
+++ js/modules/base-calculator.js
@@ -9,35 +9,48 @@ export const mainCalculator = () => {
         output.textContent += content;
     };
     refreshOutput(); */
+    operators()
 
     /* cria um evento para cada numero/tecla */
     operands.forEach(operand => operand.addEventListener('click', function(e) {
         const output = document.getElementById('output');
-         output.textContent += e.target.textContent
+        output.textContent += e.target.textContent;
     }));
 }
 
+// coloca eventos nos operadores
 const operators = () => {
     const output = document.getElementById('output');
     const plus = document.getElementById('plus-operator');
     const multiply = document.getElementById('multiply-operator');
     const minus = document.getElementById('minus-operator');
     const divide = document.getElementById('divide-operator');
+    const mod = document.getElementById('mod-operator');
+    const invert = document.getElementById('invert');
+    const decimal = document.getElementById('decimal');
+    const equal = document.getElementById('equal');
+    const backspace = document.getElementById('backspace')ç
+    const clearOutput = document.getElementById('clear-output');
 
     
     const operandsCache = [];
     operandsCache.push(output.textContent)
     console.log(operandsCache)
 
+
+
     
     
 
-    plus.addEventListener('click', insertContent);
-    minus.addEventListener('click', insertContent);
-    divide.addEventListener('click', insertContent);
-    multiply.addEventListener('click', insertContent);
+    plus.addEventListener('click', insertToOutput);
+    minus.addEventListener('click', insertToOutput);
+    multiply.addEventListener('click', insertToOutput);
+    divide.addEventListener('click', insertToOutput);
+    mod.addEventListener('click', insertToOutput);
 }
 
-const insertContent = (e) => {
-    e.target.textContent;
+// coloca os itens no monitor
+const insertToOutput = (e) => {
+    const output = document.getElementById('output');
+    return output.textContent += e.target.textContent;
 };
