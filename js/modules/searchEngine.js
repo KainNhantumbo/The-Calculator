054b41b4577d71134c77feda09ee3d22235c755f
KainNhantumbo <nhantumbok@gmail.com>
2022-02-03 08:07:56 +0200

    Adicionadas a funcionalidades de esconder ou revelar os cartoes ao pequisar.

--- js/modules/searchEngine.js
+++ js/modules/searchEngine.js
@@ -1,6 +1,7 @@
 export const search = () => {
     const cards = document.querySelectorAll('#item-title')
     const keywords = document.getElementById('search').value;
+    const cardsContainer = document.querySelectorAll('.cards-container');
     const cardTitles = new Array();
 
     cards.forEach(card => {
@@ -8,8 +9,19 @@ export const search = () => {
         cardTitles.push(cardTitle);
         /* const darto = cardTitles.match(keywords);
         console.log(darto[2]) */
+        hideCards()
+        showCards()
+        
 
     });
+
+    
+
+    const truffy = cardTitles.filter(value => {
+        
+    })
+
+    console.log(truffy)
     
     console.log(cardTitles)
 
@@ -18,6 +30,24 @@ export const search = () => {
 
     
 }
+
+// esconde os cartões ao pesquisar
+function hideCards () {
+    cardsContainer.forEach(card => {
+        card.setAttribute('style', 'visibility: hidden;');
+    });
+}
+
+// revela os cartões após a perquisa
+function showCards () {
+    if (keywords === '') {
+        cardsContainer.forEach(card => {
+            card.removeAttribute('style');
+        });
+    }
+}
+
+
 /* 
 function collectData (card) {
     
